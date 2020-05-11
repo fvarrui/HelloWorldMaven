109edfb5d6f2fc206a1ea845550f023e0bba3019
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-05-11 02:19:54 +0100

    U now it uses swing, not javafx

---
+++ src/main/java/io/github/helloworld/HelloWorldFrame.java
@@ -0,0 +1,58 @@
+package io.github.helloworld;
+
+import java.awt.FlowLayout;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+import org.apache.commons.io.FileUtils;
+
+@SuppressWarnings("serial")
+public class HelloWorldFrame extends JFrame {
+
+	public HelloWorldFrame() {
+		super("Hello World");
+		initFrame();
+        initContent();
+        setVisible(true);
+	}
+
+	public void initContent() {
+		JLabel contentLabel = new JLabel("Hello World");
+
+		JLabel pathLabel = new JLabel("Hello World");
+        
+		File info = new File("info.txt");
+		pathLabel.setText(info.getAbsolutePath());
+
+		try {
+			String content = FileUtils.readFileToString(info, Charset.forName("UTF-8"));
+			contentLabel.setText(content);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+        getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
+        getContentPane().add(contentLabel);
+        getContentPane().add(pathLabel);		
+	}
+
+	public void initFrame() {
+		setSize(320, 200);
+		setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new HelloWorldFrame();
+            }
+        });
+	}
+	
+}
--- src/main/java/io/github/helloworld/Main.java
+++ src/main/java/io/github/helloworld/Main.java
@@ -3,7 +3,7 @@ package io.github.helloworld;
 public class Main {
 
 	public static void main(String[] args) {
-		HelloWorldApp.main(args);
+		HelloWorldFrame.main(args);
 	}
 
 }
