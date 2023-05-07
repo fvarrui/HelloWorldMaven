39b66e956e1cf202fc746f907e69f677a383ba0c
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-05-07 16:11:54 +0100

    Update Main.java

--- src/main/java/io/github/fvarrui/helloworld/Main.java
+++ src/main/java/io/github/fvarrui/helloworld/Main.java
@@ -1,10 +1,15 @@
 package io.github.fvarrui.helloworld;
 
+import java.io.IOException;
 import java.lang.module.ModuleDescriptor;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		if (args.length > 0) {
 			String argument = args[0];
 			switch (argument) {
@@ -13,6 +18,9 @@ public class Main {
 				return;
 			case "--module-info":
 				moduleInfo();
+				return;
+			case "--issue-228":
+				issue228();
 				return;
 			}
 		}
@@ -28,11 +36,30 @@ public class Main {
 		if (descriptor != null) {
 			System.out.println("Modular version of HelloWorldMaven:");
 			System.out.println("- Module name : " + descriptor.name());
-			System.out.println("- Requires    : " + descriptor.requires());			
-			System.out.println("- Exports     : " + descriptor.exports());			
+			System.out.println("- Requires    : " + descriptor.requires());
+			System.out.println("- Exports     : " + descriptor.exports());
 		} else {
 			System.out.println("Non modular version of HelloWorldMaven!");
 		}
+	}
+
+	private static void issue228() {
+		try {
+			String url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl=auto&tl=zh-CNdt=t&q=" + URLEncoder.encode("word", "UTF-8");
+			URL serverUrl = new URL(url);
+			HttpURLConnection conn = (HttpURLConnection) serverUrl.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Content-type", "application/json");
+			conn.setInstanceFollowRedirects(false);
+			conn.connect();
+		} catch (Exception e) {
+			// javax.net.ssl.SSLHandshakeException: Received fatal alert: handshake_failure
+			e.printStackTrace();
+		}
+		System.out.println("Enter any character to end.");
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine();
+		scanner.close();
 	}
 
 }
